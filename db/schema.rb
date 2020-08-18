1716697fca1eddcef7fcc647ec1ab73bdc42a4ca
Darren <darren.lo.0530@gmail.com>
2020-08-17 21:58:14 -0400

    Added user reference to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_015455) do
+ActiveRecord::Schema.define(version: 2020_08_18_015618) do
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_015455) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
 end
